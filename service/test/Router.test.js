06d73212b9e0a8d01bf2a2c8431add3cc120b8b9
Harrison Harnisch <hharnisc@gmail.com>
2016-05-31 20:02:39 -0500

    handle addRole errors

--- service/test/Router.test.js
+++ service/test/Router.test.js
@@ -79,4 +79,31 @@ describe('Router', () => {
       })
       .end(done);
   });
+
+  it('does handle /addrole errors', (done) => {
+    const userId = 1;
+    const role = 'read';
+    const error = 'some error';
+    const dbDriver = {
+      addRole: jest.fn().mockImplementation(() => new Promise((resolve, reject) => reject(error))),
+    };
+    const router = new Router({ dbDriver });
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(router.router);
+    request(app)
+      .post('/addrole')
+      .send({
+        userId,
+        role,
+      })
+      .expect((res) => {
+        expect(res.status)
+          .toEqual(400);
+        expect(res.body)
+          .toEqual({ error });
+      })
+      .end(done);
+  });
 });
